3699c4e67cd3d23fcc92dd7eff84248993ad0800
Louis Scheepers <louisschprs@gmail.com>
2017-08-26 19:47:46 +0200

    Set up of User.php class basic frame

--- webservice/classes/User.php
+++ webservice/classes/User.php
@@ -1,3 +1,49 @@
 <?php 
 
-class User
+class User{
+	private $userName;
+	private $passWord;
+	
+	public function __Construct(){
+		
+	}
+	
+	public function setUserName($username){
+		$this->userName = username;
+	}
+	
+	public function getUserName(){
+		return;
+	}
+	
+	public function setPassword(){
+		
+	}
+	
+	public function getPassword(){
+		return;
+	}
+	
+	public function setName($fname, $lname){
+		
+	}
+	
+	public function getName(){
+		
+	}
+	
+	public function setBirthDate($date){
+		
+	}
+	
+	public function getBirthDate(){
+		
+	}
+	
+	public function updateReputation(){
+		
+	}
+	public function getReputation(){
+		
+	}
+}
