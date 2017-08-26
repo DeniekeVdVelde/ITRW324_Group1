0e993c6f63b37fbaf88d8d0e1d286a02b1c0b130
Braam <braamvdb96@gmail.com>
2017-08-26 16:01:48 +0200

    Ek het die connection vir webservice probeer maak en die php files is in. Die webservice connect al met die database

---
+++ webservice/login.php
@@ -0,0 +1,18 @@
+<?php
+
+	$servername = "127.0.0.1";
+	$username = "rootr";
+	$password = "324";
+	$db_name = "test";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password);
+	mysql_select_db("$db_name") or die ("no database");
+
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	echo "Connected successfully ";
+
+?>
---
+++ webservice/register.php
@@ -0,0 +1,21 @@
+<?php
+	include_once('confi.php');
+	error_reporting(E_ALL);
+	//Get the variables
+	$username = isset($_GET['username']) ?
+	mysql_real_escape_string($_GET['username']) : "";
+	$password = isset($_GET['password']) ?
+	mysql_real_escape_string($_GET['password']) : "";
+	$email = isset($_GET['email']) ?
+	mysql_real_escape_string($_GET['email']) : "";
+	//Register code here begin
+	$insertstatement = 'INSERT INTO 
+	`register`(`id`,`username`,`password`,`email`) VALUES 
+	(NULL,"'.$username.'","'.$password.'","'.$email.'")';
+	$query123 = mysql_query($insertstatement);
+	
+	
+	echo "$query123";
+	//Register code ends
+
+?>
