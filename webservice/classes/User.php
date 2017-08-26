f32f288614bd1189250ac716d7600ec6e1baef22
Louis Scheepers <louisschprs@gmail.com>
2017-08-26 20:33:40 +0200

    added more set and get functions

--- webservice/classes/User.php
+++ webservice/classes/User.php
@@ -3,6 +3,9 @@
 class User{
 	private $userName;
 	private $passWord;
+	private $email;
+	private $fName;
+	private $lName;
 	
 	public function __Construct(){
 		
@@ -13,23 +16,39 @@ class User{
 	}
 	
 	public function getUserName(){
-		return;
+		return $userName;
 	}
 	
-	public function setPassword(){
-		
+	public function setPassword($passw){
+		$this->passWord = passw;
 	}
 	
 	public function getPassword(){
-		return;
+		return $passWord;
 	}
 	
+	public function setEmail($email){
+		$this->email = email;
+	}
+	public function getEmail(){
+		return $email;
+	}
 	public function setName($fname, $lname){
-		
+		$this->fName = fname;
+		$this->lName = lname;
 	}
 	
 	public function getName(){
-		
+		$name = $this->fName . " " . $this->lName;
+		return $name;
+	}
+	
+	public function getFirstName(){
+		return $this->fName;
+	}
+	
+	public function getLastName(){
+		return $this->lName;
 	}
 	
 	public function setBirthDate($date){
