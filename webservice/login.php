312581c24b1bb927541bf9aa74754b54c78b0e1f
Braam <braamvdb96@gmail.com>
2017-09-09 18:42:57 +0200

    Validation added for admin user

--- webservice/login.php
+++ webservice/login.php
@@ -7,6 +7,35 @@
 	mysql_real_escape_string($_GET['username']) : "";
 	$password = isset($_GET['password']) ?
 	mysql_real_escape_string($_GET['password']) : "";
+		
+		
+	$insertstatement = 'SELECT count(*) co FROM `admin` WHERE
+	username="'.$username.'" AND password="'.$password.'" ';
+	
+	$query123 = mysql_query($insertstatement) or
+	trigger_error(mysql_error()." ".$insertstatement);
+	
+	while($r = mysql_fetch_array($query123)){
+		extract($r);
+	//echo "count star is $co";
+	}
+	
+	$co = (int)$co;
+	$customer_id = '';
+	if($co == 1)
+	{//user logged 
+		
+		$result = array();
+		$result[] = array("Login successful", "status" => 1);
+	}
+	
+	if($co != 1)
+	{//user not available
+		
+		$result = array();
+		$result[] = array("Login Failed", "status" => 0);
+	}
+	
 	//Login code here begin
 	$insertstatement = 'SELECT count(*) co FROM `register` WHERE
 	username="'.$username.'" AND password="'.$password.'" ';
