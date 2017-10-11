ba16bbf4f3675b88ade7a3dbb7998fd37236453f
J.P. Ferreira <jpferreira445@gmail.com>
2017-10-11 23:03:05 +0200

    Initial code streamlining and contact form funcionality finalised

---
+++ website/includes/team.php
@@ -0,0 +1,107 @@
+<!-- Team Section -->
+    <section id="team" class="bg-light-gray">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Our Amazing Team</h2>
+                    <h3 class="section-subheading text-muted">Final year BSc.IT students at NWU Potchefstroom university.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/Tjaart.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Tjaart Prinsloo</h4>
+                        <p class="text-muted">Lead Designer</p>
+                        <ul class="list-inline social-buttons">
+      
+                            <li><a href="https://www.facebook.com/TjaartPPrinsloo" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/tjaart-prinsloo-0a4942103/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+				<div class="row">
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/JP.jpg" class="img-responsive img-circle" alt="">
+                        <h4>JP Ferreira</h4>
+                        <p class="text-muted">Lead Designer</p>
+                        <ul class="list-inline social-buttons">
+                           
+                            <li><a href="https://www.facebook.com/johan.phillip.ferreira" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/jp-ferreira-92593bb6/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+				<div class="row">
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/Denieke.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Denieke van de velde</h4>
+                        <p class="text-muted">Blockchain/Android developer</p>
+                        <ul class="list-inline social-buttons">
+                            
+                            <li><a href="https://www.facebook.com/denieke.vandevelde" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/denieke-vdvelde-81858a42/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+				<div class="row">
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/Bernard.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Bernard de Klerk</h4>
+                        <p class="text-muted">Multichain</p>
+                        <ul class="list-inline social-buttons">
+                           
+                            <li><a href="https://www.facebook.com/bernard.deklerk.14" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/Braam.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Braam van der Berg</h4>
+                        <p class="text-muted">Web service</p>
+                        <ul class="list-inline social-buttons">
+                            
+                            <li><a href="https://www.facebook.com/braam.vanderberg.1" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/braam-van-der-berg-644156114/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="team-member">
+                        <img src="img/team/Louis.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Louis Scheepers</h4>
+                        <p class="text-muted">Web service</p>
+                        <ul class="list-inline social-buttons">
+                            
+                            <li><a href="https://www.facebook.com/ScheepersLouis" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="https://www.linkedin.com/in/louis-scheepers-267554b3/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                
+                </div>
+				            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <p class="large text-muted">We develop Kickass blockchain apps.</p>
+                </div>
+                
+                
+            </div>
+        </div>
+    </section>
